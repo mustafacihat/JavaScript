9903c3ab7bd202727a5640a2ec9cee77f3b181c6
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2021-03-31 14:18:08 +0200

    created fullName.js

---
+++ fullName.js
@@ -0,0 +1,8 @@
+var firstName = "Caydo";
+var lastName = "MCY";
+
+var fullName = firstName + " " + lastName;
+
+console.log(firstName);
+console.log(lastName);
+console.log(fullName);
---
+++ variables.js
@@ -0,0 +1,15 @@
+//console.log("MCY");
+
+var name;
+name = "MCY";
+
+
+console.log(name);
+
+var lastName = "Caydo";
+
+console.log(lastName);
+
+var age = 18;
+
+console.log(age);
