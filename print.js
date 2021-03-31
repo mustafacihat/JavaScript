f3be163b98c2ecfbe4460bb6ba8d5f1d015fe0c1
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2021-03-31 14:11:13 +0200

    I added new files to project

---
+++ print.js
@@ -0,0 +1,5 @@
+console.log("My name is Caydo");
+
+console.log("This is my city which I live here is Bordeaux!!!");
+
+alert("this is just for practice purposes");
---
+++ rectangleArea.js
@@ -0,0 +1,6 @@
+var width = 5;
+var length = 10;
+
+var area = width*length;
+
+console.log(area);
