e3305876cec8eaa3854609514eede7ae1bd86dcc
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2021-03-31 14:34:09 +0200

    added some codes
    added some codes to variables.js

--- variables.js
+++ variables.js
@@ -1,17 +1,30 @@
-//console.log("MCY");
-
-var name;
-name = "MCY";
-console.log(name);
-
-var lastName = "Caydo";
-console.log(lastName);
-
-var age = 18;
-console.log(age);
+// console.log("MCY");
+//
+// var name;
+// name = "MCY";
+// console.log(name);
+//
+// var lastName = "Caydo";
+// console.log(lastName);
+//
+// var age = 18;
+// console.log(age);
 
 var num1 = 1000;
 console.log(num1);
 
 var num2 = 2000;
 console.log(num2);
+
+
+num2 = num1;
+
+console.log(num1);
+console.log(num2);
+
+
+var i = 100;
+
+i = 10;
+
+console.log(i);
