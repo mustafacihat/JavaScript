e6caf88e7d7470b9b88425bb4afb921dcb4a3a03
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2021-03-31 14:28:43 +0200

    last update
    I added some codes to variables.js

--- variables.js
+++ variables.js
@@ -2,14 +2,16 @@
 
 var name;
 name = "MCY";
-
-
 console.log(name);
 
 var lastName = "Caydo";
-
 console.log(lastName);
 
 var age = 18;
-
 console.log(age);
+
+var num1 = 1000;
+console.log(num1);
+
+var num2 = 2000;
+console.log(num2);
