99d0fafeae28a1121fdc8a2e08a04d597a684cb6
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2021-04-01 23:50:05 +0200

    added new codes

--- arithmeticOperators.js
+++ arithmeticOperators.js
@@ -1,60 +1,92 @@
-//Operator: x+y
+// //Operator: x+y
+//
+// //Number + Number = Addition
+// console.log(2 + 2);
+// console.log(20 + 200);
+//
+// //Boolean + Number = Addition
+// console.log(false + 1);//1
+// console.log(true + 1);//2
+//
+// //Number + String = Concatenation
+// console.log(5 + "Hello");
+//
+// //String + Boolean = Concatenation
+// console.log("hello" + false);
+//
+// //String + String = Concatenation
+// console.log("hello " + "world");
+//
+// //Operator: x-y
+// console.log(5 - 3);
+// console.log(10 - 20);
+// console.log(-10 - 10);
+//
+// //Operator: x*y
+// console.log(10*10);
+// console.log(20*-10);
+//
+// //Operator: x/y
+// console.log(10/10);
+// console.log(20/10);
+// console.log(10*10);
+// console.log(20/0);
+// console.log(0/20);
+//
+// //Operator: x%y
+// console.log(5%3);
+// console.log(6%3);
+//
+// //Operator: x++ or ++x
+// var x = 3;
+// y=x++;
+// console.log(x);
+// console.log(y);
+//
+//
+//
+// var num1= 46;
+// var num2= 90;
+// var sum = num1+num2;
+// console.log("sum = " + sum);
+//
+// num1 = prompt("enter the value of num1");
+// num2= prompt("enter the value of num2");
+//
+// num1 = parseInt(num1);
+// num2 = parseInt(num2);
+//
+// sum= num1+num2;
+// console.log("sum = " + sum);
+//
+//
+// var celsius = prompt("please enter the celsius value");
+// console.log(typeof celsius);
+// celsius = parseInt(celsius);
+// console.log(typeof celsius);
+// var fahrenheit = celsius * 9/5 + 32;
+// console.log(typeof fahrenheit);
+//
+// console.log(celsius + " celsius is = " + fahrenheit + " fahrenheit!!!");
+//
+//
+// var miles = prompt("Enter your value in mile");
+//
+// miles = parseInt(miles);
+//
+// var km = miles * 1.609344;
+// console.log(miles + " miles " + km + " km");
 
-//Number + Number = Addition
-console.log(2 + 2);
-console.log(20 + 200);
+console.log(3+5*4-6);
 
-//Boolean + Number = Addition
-console.log(false + 1);//1
-console.log(true + 1);//2
-
-//Number + String = Concatenation
-console.log(5 + "Hello");
-
-//String + Boolean = Concatenation
-console.log("hello" + false);
-
-//String + String = Concatenation
-console.log("hello " + "world");
-
-//Operator: x-y
-console.log(5 - 3);
-console.log(10 - 20);
-console.log(-10 - 10);
-
-//Operator: x*y
-console.log(10*10);
-console.log(20*-10);
-
-//Operator: x/y
-console.log(10/10);
-console.log(20/10);
-console.log(10*10);
-console.log(20/0);
-console.log(0/20);
-
-//Operator: x%y
-console.log(5%3);
-console.log(6%3);
-
-//Operator: x++ or ++x
-var x = 3;
-y=x++;
-console.log(x);
-console.log(y);
+var a = 50;
+sum = --a + a++ + a-- + a++;
+console.log(a);
+console.log(sum);
 
 
+a = 1;
+sum = -a-- + a++ / -a-- * --a;
 
-var num1= 46;
-var num2= 90;
-var sum = num1+num2;
-console.log("sum = " + sum);
-
-num1 = prompt("enter the value of num1");
-num2= prompt("enter the value of num2");
-
-num1 = parseInt(num1);
-num2 = parseInt(num2);
-
-sum= num1+num2;
-console.log("sum = " + sum);
+console.log(sum);
+console.log(a);
