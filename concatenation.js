994e510df7b38d7371a8c51c50e268fa0e720a54
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2021-04-01 02:23:24 +0200

    update
    updated project

---
+++ concatenation.js
@@ -0,0 +1,30 @@
+
+// var today ;
+// today = prompt("What is today?")
+// console.log("Today is " + today);
+//
+// var greeting;
+// greeting = "Hello World";
+// console.log(greeting + ", Today is " + today);
+
+
+// var name;
+// name = prompt("What is your name?");
+//
+// var jobTitle;
+// jobTitle= prompt("What is your job title?")
+//
+// console.log(name +  " is very good and his job title is " + jobTitle + "!");
+
+
+var className = "Chemistry";
+var part = "-5";
+var classAll = className + part;
+
+console.log(classAll);
+
+
+var firstName = prompt("Please enter your name");
+var lastName = prompt("Please enter your last name");
+var fullName = firstName + " " + lastName;
+console.log("Your full name is " + fullName);
