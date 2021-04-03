70464d6cefaaa9e6b37fb4fd5b4f31e068111d8b
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2021-04-04 01:01:34 +0200

    last update

---
+++ promptExercise.js
@@ -0,0 +1,8 @@
+var firstName = prompt("Please enter your first name");
+
+var lastName = prompt("Please enter your last name");
+
+var age = prompt("Please enter your age");
+
+console.log("Hi, my name is " + firstName + " " + lastName + ", and my age is "
++ age + ". Nice to meet you !");
