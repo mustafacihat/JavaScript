a17799cd99c50379de16d52f5be0e19a505283e0
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2021-04-11 02:09:29 +0200

    last update

---
+++ ClassesFiles/forLoop.js
@@ -0,0 +1,28 @@
+console.log("javascript");
+console.log("javascript");
+console.log("javascript");
+console.log("javascript");
+console.log("javascript");
+
+
+for (var i = 0; i < 5; i++) {
+    console.log("js");
+}
+
+
+for (var i = 0; i <= 10; i += 2) {
+    console.log(i)
+}
+
+for (var i = 0; i < 51; i++) {
+    if (i % 4 == 0) {
+        console.log(i);
+    }
+}
+console.log("====================================")
+
+for (var i = 0; i < 51; i += 4) {
+    if (i % 4 == 0) {
+        console.log(i);
+    }
+}
