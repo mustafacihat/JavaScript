a6593c7320795852c9a5b19ec996dbf955530797
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2021-04-18 22:49:11 +0200

    last update

---
+++ ClassesFiles/multiplicationTable.js
@@ -0,0 +1,7 @@
+var num = parseInt(prompt("Enter a number please"));
+
+for (var i = 1; i <= 10; i++) {
+
+    var result = i * num;
+    console.log(num + " * " + i + " = " + result);
+}
---
+++ ClassesFiles/sumOfFive.js
@@ -0,0 +1,10 @@
+var sum=0;
+for (let i = 1; i <= 5; i++) {
+
+    var num = parseInt(prompt("Please enter the " + i + ". number"));
+    sum += num
+    console.log(sum)
+}
+console.log("This is the result : " + sum)
+
+
